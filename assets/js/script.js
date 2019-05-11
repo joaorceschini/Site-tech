7d96392c603ff4a1c9a7ae724d5c886f161b5319
João Rafael Valarini R. Ceschini <jrvceschini@gmail.com>
2019-05-11 17:55:02 -0300

    Consertei o banner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,21 @@
-document.querySelector('.open-menu').addEventListener('click', e => {
 
-    document.querySelector('header .menu').classList.add('open');
-
-});
-
-document.querySelector('.close-menu button').addEventListener('click', e => {
-
-    document.querySelector('header .menu').classList.remove('open');
-
-});
-
-document.querySelector('.menu .backdrop').addEventListener('click', e => {
-
-    document.querySelector('header .menu').classList.remove('open');
-
-});
 
 ////////////////////////////////////////////////////////////
 
-var time = window.setInterval(change, 1000)
 
 var count = 0;
 
 function change(){
     count++
     let template = `#banner-${count}`;
-    let present = document.querySelector(template)
+    let present = document.querySelector(template);
 
     let b = count-1;
-    let previous = document.querySelector(`#banner-${b}`)
+    let previous = document.querySelector(`#banner-${b}`);
 
     if(b <= 0){
         b++
-        previous = document.querySelector(`#banner-4`)
+        previous = document.querySelector(`#banner-4`);
     }
     if(count >= 4){
         count = 0;
@@ -46,20 +29,14 @@ function change(){
 
     previous.classList.remove("banner-show");
     previous.style.opacity = 0;
-    //present.classList.remove("banner-hidden");
-    //previous.classList.add("banner-hidden");
 
 }
+
+var time = window.setInterval(change, 2500)
 
 document.addEventListener('copy', e => {
 
     alert("Lembre-se de usar os direitos autorais! ;)");
-
-})
-
-document.addEventListener('transitionend', e => {
-
-    console.log("Lembre-se de usar os direitos autorais! ;)");
 
 })
 
